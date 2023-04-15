92148874b577740fa085ebf7ec28d338b6cec7e8
Chirag Pandey <pndy.chrg2003@gmail.com>
2023-04-16 01:21:50 +0530

    Added JWT Token Auth

--- resources/user.py
+++ resources/user.py
@@ -1,11 +1,34 @@
-from flask_restful import Resource,reqparse,abort
+from flask_restful import Resource,reqparse,abort,request
 from models.userModel import User
 from database.userDB import UserDatabaseFunctions
-
-
+from flask import jsonify
+import jwt
+from functools import wraps
+from run import app
 # initiating userDatabseFunctions
 userDatabaseFunctions = UserDatabaseFunctions()
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = userDatabaseFunctions.get_UserbyUsername(username=data['username'])
+        except:
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users context to the routes
+        return  f(current_user, *args, **kwargs)
+    return decorated
 
 class UserRegistration(Resource):
     def post(self):
@@ -19,10 +42,11 @@ class UserRegistration(Resource):
         new_user = User(name=data['name'],username=data['username'],password=data['password'])
         id = userDatabaseFunctions.add_User(user=new_user)
         if id != False:
-            return {"id":id}
+            return {"message":"User Registered","id":id}
         return {'message':"User already Exists"},400
         
 class UserLogin(Resource):
+    
     def post(self):
         # parser for login
         parser = reqparse.RequestParser()
@@ -31,10 +55,9 @@ class UserLogin(Resource):
         data = parser.parse_args()
         user_db = userDatabaseFunctions.get_UserbyUsername(username= data['username'])
         if user_db != None:
-            print(data,flush=True)
-            print(user_db.password,flush=True)
             if(user_db.username==data['username'] and user_db.password==data['password']):
-                return {'message':"User Login"},200
+                token = jwt.encode({'username':user_db.username},app.config['SECRET_KEY'])
+                return {'message':"User Login","token":token},200
             else:
                 return {'message':"Wrong Credentials"},401
         return {'message':"User doesn't exist"},401
--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_restful import Api
-from resources.user import *
 from database.db import init_Database
 
 #init
 app = Flask(__name__)
+app.config['SECRET_KEY']="flask_api_for_ticket_provider"
 import views
-api = Api(app)
 
+api = Api(app)
+from resources.user import *
 api.add_resource(UserLogin,'/login')
 api.add_resource(UserRegistration,'/register')
 api.add_resource(UserLogoutAccess,'/logout')
