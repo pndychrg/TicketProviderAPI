88c005c11e14143fa913d19399f43bbc7b819aba
Chirag Pandey <pndy.chrg2003@gmail.com>
2023-05-03 14:48:58 +0530

    requirements.txt added

--- src/run.py
+++ src/run.py
@@ -2,11 +2,34 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from database.db import init_Database
+# from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'http://petstore.swagger.io/v2/swagger.json'
 
 #init
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY']="flask_api_for_ticket_provider"
 jwt = JWTManager(app)
+
+# swaggerui_blueprint = get_swaggerui_blueprint(
+#     SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+#     API_URL,
+#     config={  # Swagger UI config overrides
+#         'app_name': "Test application"
+#     },
+#     # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+#     #    'clientId': "your-client-id",
+#     #    'clientSecret': "your-client-secret-if-required",
+#     #    'realm': "your-realms",
+#     #    'appName': "your-app-name",
+#     #    'scopeSeparator': " ",
+#     #    'additionalQueryStringParams': {'test': "hello"}
+#     # }
+# )
+
+# app.register_blueprint(swaggerui_blueprint)
+# #Now point your browser to localhost:5000/api/docs/
 import views
 
 api = Api(app)
