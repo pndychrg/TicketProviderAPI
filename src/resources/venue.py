c5ff3b7d019829ccae8e00b0926d24b35b9323a5
lakhan2506 <82992503+lakhan2506@users.noreply.github.com>
2023-11-10 15:40:10 +0530

    updated

--- src/resources/venue.py
+++ src/resources/venue.py
@@ -2,25 +2,44 @@ from flask_restful import Resource,reqparse,request
 from flask_jwt_extended import jwt_required
 from models.venueModel import Venue
 from database.venueDB import VenueDB
+from flask import jsonify
+import json
 
 #initialising Venue Database Functions
 venueDB = VenueDB()
 
 class AddVenue(Resource):
+    def options(self):
+        response = jsonify({'message': 'Preflight request accepted.'})
+        #response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        return response
     @jwt_required()
     def post(self):
         # parser for adding venue details
-        parser = reqparse.RequestParser()
-        parser.add_argument('name',help='This Field is mandatory',required = True)
-        parser.add_argument('place',help="This Field is mandatory",required = True)
-        parser.add_argument('capacity',help="This Field is mandatory",required = True)
+        # parser = reqparse.RequestParser()
+        # parser.add_argument('name',help='This Field is mandatory',required = True)
+        # parser.add_argument('place',help="This Field is mandatory",required = True)
+        # parser.add_argument('capacity',help="This Field is mandatory",required = True)
         
-        # extracting data
-        data = parser.parse_args()
+        # # extracting data
+        # data = parser.parse_args()
+        data = request.get_json()
+        print("data first ",data,flush=True)
+        if isinstance(data,str):
+            try:
+                data = json.loads(data)
+            except json.JSONDecodeError:
+                return {'message': 'Invalid JSON data in the request body'}, 400
+        if "name" not in data or "place" not in data or "capacity" not in data:
+            return {'message':'Invalid JSON '},400
+        print(data,flush=True)
         new_venue = Venue(name=data['name'],place=data['place'],capacity=data['capacity'])
         id = venueDB.addVenue(venue=new_venue)
         if id!=None:
-            return {'message':"Venue Added"}
+            print('Venue Added')
+            return {'message':"Venue Added"},200
         return {"message":"Error Occured"},404
     
 class GetAllVenue(Resource):
--- src/run.py
+++ src/run.py
@@ -52,7 +52,6 @@ def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  print("origin added",flush=True)
   return response
 
 
